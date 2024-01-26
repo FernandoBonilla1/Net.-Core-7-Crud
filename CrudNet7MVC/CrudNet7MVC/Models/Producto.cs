411804744bae3c2df72759043236c86dc2b926e8
Fernando Bonilla <39638041+FernandoBonilla1@users.noreply.github.com>
2024-01-26 10:57:01 -0300

    Proyecto Crud

---
+++ CrudNet7MVC/CrudNet7MVC/Models/Producto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudNet7MVC.Models
+{
+    public class Producto
+    {
+        public int ProductoId { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El precio es obligatorio")]
+        public double Precio { get; set; }
+    }
+}
